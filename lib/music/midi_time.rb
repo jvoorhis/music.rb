76d58d4ab17faf4383041e9dd36094ccc9d91c92
Jeremy Voorhis <jvoorhis@jeremy-voorhiss-macbook.local>
2008-07-27 13:03:59 -0700

    Moved Music::SMFWriter and Music::MidiTime to their own files.

---
+++ lib/music/midi_time.rb
@@ -0,0 +1,18 @@
+module Music  
+  class MidiTime
+    attr :resolution
+    
+    def initialize(res)
+      @resolution = res
+    end
+    
+    def ppqn(val)
+      case val
+        when Numeric
+          (val * resolution).round
+        else
+          raise ArgumentError, "Cannot convert #{val}:#{val.class} to midi time."
+      end
+    end
+  end
+end
---
+++ lib/music/smf_writer.rb
@@ -0,0 +1,43 @@
+require 'smf'
+require 'music/midi_time'
+
+module Music
+  # Standard Midi File performance.
+  class SMFWriter
+    include SMF
+    
+    def initialize(options={})
+      @time = MidiTime.new(options.fetch(:resolution, 480))
+      @seq  = Sequence.new(1, @time.resolution)
+    end
+    
+    def track(performance, options={})
+      @track = Track.new
+      seq_name = options.fetch(:name, gen_seq_name)
+      @track << SequenceName.new(0, seq_name)
+      @channel = options.fetch(:channel, 1)
+      
+      performance.each do |event|
+        attack  = @time.ppqn(event.time)
+        release = attack + @time.ppqn(event.object.duration)
+        @track << NoteOn.new(attack, @channel, Music.MidiPitch(event.object.pitch), event.object.effort)
+        @track << NoteOff.new(release, @channel, Music.MidiPitch(event.object.pitch), event.object.effort)
+      end
+      
+      @seq << @track
+      self
+    end
+    
+    def save(basename)
+      filename = basename + '.mid'
+      @seq.save(filename)
+    end
+    
+    protected
+      def gen_seq_name
+        @seqn ||= 0
+        @seqn  += 1
+        "Untitled #@seqn"
+      end
+  end
+end
