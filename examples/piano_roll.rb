e384d7d167c0a6a6ebfb4533aa2547927b8cf441
Jeremy Voorhis <jvoorhis@jeremy-voorhiss-macbook-2.local>
2008-08-23 19:36:47 -0700

    Namespaced xml for PianoRoll; embeddable in a web page.

--- examples/piano_roll.rb
+++ examples/piano_roll.rb
@@ -4,29 +4,30 @@ require 'builder'
 
 class PianoRoll
   def initialize(music, options = {})
-    @timeline = music.to_timeline
-    @duration = music.duration.to_f
-    @w        = options.fetch(:width, 24.0)
-    @h        = options.fetch(:height, 12.0)
-    @minp     = @timeline.map(&:pitch).min
-    @maxp     = @timeline.map(&:pitch).max
-    @rangep   = @maxp - @minp
-    @xml      = Builder::XmlMarkup.new(:indent => 2)
+    @timeline   = music.to_timeline
+    @duration   = music.duration.to_f
+    @w          = options.fetch(:width, 24.0)
+    @h          = options.fetch(:height, 12.0)
+    ps          = @timeline.map(&:pitch)
+    @minp       = ps.min
+    @maxp       = ps.max
+    @rangep     = @maxp - @minp
+    @xml        = Builder::XmlMarkup.new(:indent => 2)
+    @standalone = options.fetch(:standalone, true)
     
     draw
   end
   
-  def to_s
-    @xml.target!
-  end
+  def to_s; @xml.target! end
   
   private
     def draw
-      @xml.instruct!
-      @xml.svg('xmlns'   => 'http://www.w3.org/2000/svg',
-               'version' => '1.1',
-               'width'   => cm(@w),
-               'height'  => cm(@h)) do
+      @xml.instruct! if @standalone
+      @xml.svg(:svg,
+               'xmlns:svg' => 'http://www.w3.org/2000/svg',
+               'version'   => '1.1',
+               'width'     => cm(@w),
+               'height'    => cm(@h)) do
         draw_grid
         draw_notes
       end
@@ -39,11 +40,12 @@ class PianoRoll
         w    = @w
         h    = @h * 1.quo(@rangep)
         fill = if (p % 2).zero? then '#ddddff' else '#d8d8d8' end
-        @xml.rect('x'      => cm(x),
-                  'y'      => cm(y),
-                  'width'  => cm(w),
-                  'height' => cm(h),
-                  'fill'   => fill)
+        @xml.svg(:rect,
+                 'x'      => cm(x),
+                 'y'      => cm(y),
+                 'width'  => cm(w),
+                 'height' => cm(h),
+                 'fill'   => fill)
       end
     end
     
@@ -54,15 +56,16 @@ class PianoRoll
         w    = @w * (n.duration / @duration)
         h    = @h * 1.quo(@rangep)
         fill = '#ff0000'
-        @xml.rect('x'      => cm(x),
-                  'y'      => cm(y),
-                  'width'  => cm(w),
-                  'height' => cm(h),
-                  'fill'   => fill)
+        @xml.svg(:rect,
+                 'x'      => cm(x),
+                 'y'      => cm(y),
+                 'width'  => cm(w),
+                 'height' => cm(h),
+                 'fill'   => fill)
       end
     end
     
-    def cm(n); "%2.2fcm" % n.to_f end
+    def cm(n) "%2.2fcm" % n.to_f end
 end
 
 if __FILE__ == $0
