5cf79915dda155d4e8348750d731739c85ac1e60
Jeremy Voorhis <jvoorhis@jeremy-voorhiss-macbook-2.local>
2008-11-10 08:21:24 -0800

    Now running with the midilib gem as a dependency.

--- lib/music/smf_writer.rb
+++ lib/music/smf_writer.rb
@@ -1,43 +1,44 @@
-require 'smf'
-require 'music/midi_time'
+require 'rubygems'
+require 'midilib'
 
 module Music
   class SMFWriter
-    include SMF
+    include ::MIDI
     
     def initialize(options = {})
-      @time  = MidiTime.new(options.fetch(:resolution, 480))
-      @seq   = Sequence.new(1, @time.resolution)
-      @tempo = SetTempo.new(1, bpm_to_qn_per_usec(options[:tempo]))
+      @seq   = Sequence.new
+      @tempo = Tempo.new(Tempo.bpm_to_mpq(options[:tempo]))
     end
     
     def track(arrangement_or_timeline, options = {})
       timeline = arrangement_or_timeline.to_timeline
-      track    = Track.new
+      track    = Track.new(@seq)
       channel  = options.fetch(:channel, 1)
       name     = options.fetch(:name, gen_seq_name)
       
-      track << SequenceName.new(0, name)
-      track << @tempo
+      track.events << MetaEvent.new(META_SEQ_NAME, name)
+      track.events << @tempo
       
       timeline.each_with_time do |note, time|
-        attack   = @time.ppqn(time)
-        release  = attack + @time.ppqn(note.duration)
+        attack   = @seq.length_to_delta(time)
+        release  = @seq.length_to_delta(note.duration)
         pitch    = note.pitch.to_i
         velocity = note.attributes.fetch(:velocity, 80)
         
-        track  << NoteOn.new(attack, channel, pitch, velocity)
-        track  << NoteOff.new(release, channel, pitch, velocity)
+        track.merge [
+          NoteOnEvent.new(channel, pitch, velocity, attack),
+          NoteOffEvent.new(channel, pitch, velocity, release)
+        ]
       end
       
-      @seq << track
+      @seq.tracks << track
       
       self
     end
     
     def save(basename)
-      filename = basename + '.mid'
-      @seq.save(filename)
+      filename = 'midilib' + basename + '.mid'
+      open(filename, 'wb') { |f| @seq.write(f) }
     end
     
     protected
@@ -45,10 +46,6 @@ module Music
         @seqn ||= 0
         @seqn  += 1
         "Untitled #@seqn"
-      end
-      
-      def bpm_to_qn_per_usec(bpm)
-        (60_000_000.0 / bpm).to_i
       end
   end
 end
